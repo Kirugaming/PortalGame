81e7a8cb2244c9dec9d89e614b84e7ab9292397c
LoveTheBirds <113379425+LoveTheBirds@users.noreply.github.com>
2023-01-17 12:21:34 -0600

    Fixed the door opening script

--- Assets/Scripts/BigOpenDoor.cs
+++ Assets/Scripts/BigOpenDoor.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class BigOpenDoor : MonoBehaviour
 {
     public GameObject door;
-    private Vector3 oldPos;
+    public Vector3 oldPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        oldPos = door.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -22,7 +22,6 @@ public class BigOpenDoor : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("Button Pressed");
-        oldPos = door.transform.localPosition; 
         door.transform.localPosition = new Vector3(100, 0, 0);
         //Destroy(door);
     }
